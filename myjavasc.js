adf1429ec9f71f872762884caefefb6a59797266
14FU <fauzia53.u@gmail.com>
2022-09-28 15:00:17 +0100

    task three compleetde

--- myjavasc.js
+++ myjavasc.js
@@ -1,5 +1,18 @@
-
+'use strict'
+// task one on qa functions 
 function myFunc(num1,num2){
     return num1 -num2
 }
 console.log(myFunc(40,65));// add the value inside of the brackets 
+
+//task 2 
+const welcome = function(name,age,gender){ // need to constrcut the string 
+    console.log(`my nameis ${name},i am ${age} and my gender is ${gender}`);
+}
+welcome("Anna Paul",24,"Female");
+
+//task 3
+const powerup = (n1,n2) => Math.pow(n1,n2);
+    console.log(powerup(3,2));
+
+
