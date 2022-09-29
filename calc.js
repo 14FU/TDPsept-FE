23fb6ca1bc5c9aadf5a4aed5a0677b664e1757dc
unknown <fauzia53.u@gmail.com>
2022-09-29 16:26:46 +0100

    calc task

---
+++ calc.js
@@ -0,0 +1,17 @@
+const calc = (n1,n2,op) =>{
+    let vari;
+    switch (op){
+        case "+":
+        vari = n1 +n2;
+        break;
+        case "-":
+        vari = n1-n2;
+        break;
+        case "*":
+        vari = n1 *n2;
+        break;
+        case "/":
+        vari = n1/n2;
+        break;
+    }
+}
